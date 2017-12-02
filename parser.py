fc61e376b9a18be00efa2406366fc1e6e594c983
Jordan Pizza <Pizzaface@users.noreply.github.com>
2017-12-02 11:07:49 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import csv
 import glob, os
+import random
 
 f1 = file('LowestFares.csv', 'r')
 c1 = csv.reader(f1)
@@ -15,12 +16,11 @@ possibleDeals = []
 for row in c1:
 	if str(row[4]) == "LOWEST":
 		if str(row[0].lower()) == str(starting_airport).lower():
-			print("WOOP")
 			flightInfo = {
 				"from": row[0],
 				"to": row[1],
 				"flightDate": row[2],
-				"totalPrice": row[5] + row[6],
+				"totalPrice": float(row[5]) + float(row[6]),
 				"score": row[7],
 				"domestic": row[9]
 			}
@@ -28,9 +28,30 @@ for row in c1:
 	else:
 		pass
 
-def getHighestScore(possibleDeals):
+def getBestDeal(possibleDeals):
+	highestScore = 0
+	bestFlight = None
 	for flight in possibleDeals:
+		print flight['score']
 		if flight['score'] == 0:
 			possibleDeals.remove(flight)
-	
-	print(possibleDeals)
+			continue
+		elif int(flight['score']) > highestScore:
+			highestScore = flight['score']
+			bestFlight = flight
+
+	if highestScore == 0:
+		lowestPrice = 100000
+		for flight in possibleDeals:
+			if flight['totalPrice'] == 0:
+				possibleDeals.remove(flight)
+				continue
+			elif float(flight['totalPrice']) < float(lowestPrice):
+				print("NEW LOW -- WAS: " + str(lowestPrice) + " NOW: " + str(flight['totalPrice']))
+				lowestPrice = flight['totalPrice']
+				bestFlight = flight
+
+	#print(possibleDeals)
+	return bestFlight
+
+print getRandomDeal(possibleDeals)
